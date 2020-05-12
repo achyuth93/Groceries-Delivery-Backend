af5474372c77308f71d7d4768eccb8914221a9d6
Achyuth <achyuth12693@gmail.com>
2020-05-12 09:57:03 +0530

    first commit

---
+++ src/main/java/com/Groceries/GroceriesBackend/GroceriesBackendApplication.java
@@ -0,0 +1,13 @@
+package com.Groceries.GroceriesBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroceriesBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroceriesBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Groceries/GroceriesBackend/GroceriesBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Groceries.GroceriesBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroceriesBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
